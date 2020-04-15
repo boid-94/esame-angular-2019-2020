2e752c61a0688a388a1ebcd34cd65146c8e67eb3
dorotea-iob <iobdorotea94@gmail.com>
2020-04-15 22:39:57 +0200

    ecco qua che lavorone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'toDoList';
+  model = {
+    addItem: ''
+  }
+
+  list = [];
+
+  onSubmit(addItem: any) {
+    console.log(this.model.addItem)
+    this.list.push(addItem)
+  }
+
+  remove(addedItem: number) {
+    let index = this.list.indexOf(addedItem)
+    this.list.splice(index, 1)
+  }
+
+  ngOnInit() { }
 }
